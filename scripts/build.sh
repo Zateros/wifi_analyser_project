daed1b6a5bd4eb37e2d2c6a3f27d3dfee3029716
Zateros <zazatoth92@proton.me>
2025-10-25 17:12:36 +0200

    I am sure it will work now

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pyinstaller ../gui.py --add-data "../media/floor_template.svg:." --add-data "../media/mouse_right_click.png:." --onefile --windowed -n wifianalyser
+pyinstaller ../gui.py --add-data "../media/floor_template.svg:./media" --add-data "../media/mouse_right_click.png:./media" --onefile --windowed -n WifiAnalyser
